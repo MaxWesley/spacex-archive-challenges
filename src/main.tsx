958c6d658bc1356ae65e00de2ae63259741e61b9
Max Wesley <mx_wesley@hotmail.com>
2026-03-24 19:57:40 -0300

    feat: integrate @tanstack/react-query for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import "./index.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
