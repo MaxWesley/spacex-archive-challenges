846f454ef0179a1fdd8836d33e6bdfd8c8985ef7
Max Wesley <mx_wesley@hotmail.com>
2026-03-24 20:56:08 -0300

    feat: add launch service and axios instance for API calls
    - Created a new service for fetching launches with pagination and filtering options.
    - Implemented a reusable axios instance with a base URL for API requests.

---
+++ src/features/launches/services/launche.service.ts
@@ -0,0 +1,52 @@
+import { api } from "@/lib/axios";
+
+interface GetLaunchesParams {
+  page?: number;
+  limit?: number;
+  search?: string;
+  success?: boolean;
+  upcoming?: boolean;
+}
+
+export async function getLaunches({
+  page = 1,
+  limit = 12,
+  ...params
+}: GetLaunchesParams) {
+  const query: any = {};
+
+  if (params.search) {
+    query.search = params.search;
+  }
+
+  if (params.success !== undefined) {
+    query.success = params.success;
+  }
+
+  if (params.upcoming !== undefined) {
+    query.upcoming = params.upcoming;
+  }
+
+  const { data } = await api.post("/launches/query", {
+    query,
+    options: {
+      page,
+      limit,
+      sort: {
+        date_utc: "desc",
+        populate: [
+          {
+            path: "rocket",
+            select: ["name", "type"],
+          },
+          {
+            path: "launchpad",
+            select: ["name", "locality"],
+          },
+        ],
+      },
+    },
+  });
+
+  return data;
+}
