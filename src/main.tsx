4559897a66cc921fd962b3b49e2f9b0bede85928
Max Wesley <mx_wesley@hotmail.com>
2026-03-25 03:10:05 -0300

    chore: remove index.css file to streamline styling management

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App.tsx"
-import { Provider } from "./components/ui/provider.tsx"
-import "./index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { Provider } from "./components/ui/provider.tsx";
 
-import { QueryClientProvider } from "@tanstack/react-query"
-import { queryClient } from "./lib/react-query"
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -18,4 +17,4 @@ createRoot(document.getElementById("root")!).render(
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
-)
+);
