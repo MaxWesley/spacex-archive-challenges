426e9109b6a8e2ebf2de609e49ae59df64ceb795
Max Wesley <mx_wesley@hotmail.com>
2026-03-25 03:31:00 -0300

    feat: add LaunchPageSkeleton component for loading state in LaunchesPage

---
+++ src/features/launches/components/launche-page.skeleton.tsx
@@ -0,0 +1,41 @@
+import { Box, Center, Container, Grid, Flex, Skeleton, Text } from "@chakra-ui/react";
+
+export function LaunchPageSkeleton() {
+  return (
+    <Container py={4} maxW="full" w="full" border="none">
+      <Box>
+        <Grid
+          gap={4}
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)",
+            xl: "repeat(6, 1fr)",
+          }}
+        >
+          {Array.from({ length: 12 }).map((_, idx) => (
+            <Box key={idx} p={4} borderWidth="1px" minH={300} borderRadius="xs">
+              <Center>
+                <Flex flexDir="column" alignItems="center" w="full">
+                  <Skeleton borderRadius="xs" minW={160} minH={160} />
+                  <Box mt={8} w="100%">
+                    <Flex mb={4} justifyContent="space-between" alignItems="center">
+                      <Skeleton height="20px" width="90px" borderRadius="md" />
+                      <Skeleton height="14px" width="70px" borderRadius="md" />
+                    </Flex>
+                    <Text fontWeight="bold" textAlign="left" mb={2}>
+                      <Skeleton height="18px" width="80%" borderRadius="md" />
+                    </Text>
+                    <Text fontWeight="light" fontSize="sm" textAlign="left">
+                      <Skeleton height="14px" width="90%" borderRadius="md" />
+                    </Text>
+                  </Box>
+                </Flex>
+              </Center>
+            </Box>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
--- src/features/launches/pages/launches-page.tsx
+++ src/features/launches/pages/launches-page.tsx
@@ -1,30 +1,29 @@
 import { useLaunches } from "../hooks/use-launches";
-import { Grid, Container, Box } from "@chakra-ui/react";
+import { Grid, Container } from "@chakra-ui/react";
 import { LaunchCard } from "../components/launch-card";
+import { LaunchPageSkeleton } from "../components/launche-page.skeleton";
 
 export function LaunchesPage() {
   const { data, isLoading, error } = useLaunches({ page: 1 });
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <LaunchPageSkeleton />;
   if (error) return <p>Error...</p>;
 
   return (
     <Container py={4} maxW="full" w="full" border="none">
-      <Box>
-        <Grid
-          gap={4}
-          templateColumns={{
-            base: "repeat(1, 1fr)",
-            md: "repeat(2, 1fr)",
-            lg: "repeat(3, 1fr)",
-            xl: "repeat(6, 1fr)",
-          }}
-        >
-          {data?.docs.map((launch) => {
-            return <LaunchCard key={launch.id} launch={launch} />;
-          })}
-        </Grid>
-      </Box>
+      <Grid
+        gap={4}
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+          xl: "repeat(6, 1fr)",
+        }}
+      >
+        {data?.docs.map((launch) => {
+          return <LaunchCard key={launch.id} launch={launch} />;
+        })}
+      </Grid>
     </Container>
   );
 }
